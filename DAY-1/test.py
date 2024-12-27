92ffc2a932e5720f5ed9770a9ad9528df3de83b6
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-27 14:56:07 +0530

    inbuilt_function

--- DAY-1/test.py
+++ DAY-1/test.py
@@ -1,3 +1,21 @@
+#split inbuilt fuction
 arn = "arn:aws:iam::123456789012:user/johndoe"
 
 print(arn.split("/"))
+
+#Add two strings - String Concatination
+str1 ="Hello"
+str2 ="World"
+
+result = str1 + " " + str2
+print(result)
+
+#String Length
+text = "python is awesome"
+length = len(text)
+print("Length of the string:",length)
+
+#String Strip
+text = " some spaces around "
+stripped_text = text.strip()
+print("stripped_text:",stripped_text)
