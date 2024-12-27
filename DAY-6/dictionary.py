3db30955b813fc9ffee828df3cc9d5232a8b8350
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-27 20:53:31 +0530

    adding dictionary, sets, user_input

---
+++ DAY-6/dictionary.py
@@ -0,0 +1,12 @@
+#key :value
+marks={'hindi':80,'english':90}
+print(marks)
+print(marks['hindi'])
+
+#Adding item to dictionary
+marks['maths']=100
+print(marks)
+
+#Delete item from dictionary
+del marks['hindi']
+print(marks)
