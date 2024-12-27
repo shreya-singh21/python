9117293553ab35b8d3db5d23b25e27c5db7a8f1d
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-27 17:05:57 +0530

    variables

---
+++ DAY-2/variable.py
@@ -0,0 +1,10 @@
+length_of_rect = 30 #integer
+width_of_rect = 20
+price=54.36 #Floating points
+mybol=True #boolean
+username = "shreya" #string
+
+print(f"Area of rectangle:{length_of_rect * width_of_rect}")
+print(username)
+
+print(2**3)  #2^3 power
