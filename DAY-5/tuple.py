ddd6e96018740a5e8b1ba0b52b7ea55b3147dfb8
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-27 19:37:25 +0530

    tuple

---
+++ DAY-5/tuple.py
@@ -0,0 +1,10 @@
+#same as list, however you can't modify the items of tuple.
+#Immutable
+#Ordered
+#days=('mon','tue','wed')
+
+days=('mon','tues','wed','mon')
+print(days)
+
+#count: number of item
+print(days.count('mon'))
