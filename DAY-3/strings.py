4086ee91f8b13a193cb77ad13135b4bf19d62462
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-27 17:38:29 +0530

    strings

---
+++ DAY-3/strings.py
@@ -0,0 +1,15 @@
+f_name='Paul'
+l_name='Philip'
+
+full_name=f_name +" "+l_name
+print(full_name.upper())  #change to upper case
+print(full_name.lower())  #change to lower case
+
+msg='hello world'    
+print(msg.title())    #make first character in upper case
+
+msg='hello world! is myfirst program'
+print(msg.split('!')) #split by '!'
+
+print(msg[-1]) #indexing from backside
+print(msg[0:5])
