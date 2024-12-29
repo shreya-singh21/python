722b34643b80f276fdb8b400ab9ee365be6d617c
Shreya Singh <72350545+shreya-singh21@users.noreply.github.com>
2024-12-29 12:52:05 +0530

    fizz buzz

--- DAY-11/fizz_buzz.py
+++ DAY-11/fizz_buzz.py
@@ -3,6 +3,18 @@ till_num=int(input('Enter the specified number:'))
 my_list=[]
 
 for num in range(1,till_num+1):
-    my_list.append(num)
+    result=""
+    if num%3==0:
+        result=result+'fizz'
+        if num%5==0:
+            result=result+'buzz'
+    elif num%5==0:
+        result=result+'buzz'
+    else:
+        result=num
+
+    #print(num)
+    my_list.append(result)
 
 print(my_list)
+#   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
